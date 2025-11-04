84367f3fc1790698f8d15cad1d28ba631171bd8d
Jeongyounghyeon <jeongyounghyeon1106@gmail.com>
2025-11-04 17:42:54 +0900

    chore: init commit

---
+++ src/main/java/im/fooding/FoodingGatewayApplication.java
@@ -0,0 +1,13 @@
+package im.fooding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodingGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/im/fooding/FoodingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package im.fooding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
